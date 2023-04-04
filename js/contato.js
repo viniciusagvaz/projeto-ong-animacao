ea2b10faa17154075439b71246195e1a3cc4347d
zoomviex <agvazvinicius@gmail.com>
2023-04-04 20:00:07 -0300

    bobagenzinhas

---
+++ js/contato.js
@@ -0,0 +1,8 @@
+const emailDiv = document.querySelector("contato-formulario-inputs");
+const emailInfos = document.querySelectorAll('.info')
+
+function enviar() {
+  window.alert("Mensagem enviada!");
+}
+
+
